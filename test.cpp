a70ccb94e9e136d07cd708fb00c6af1326a7ccd6
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-10-23 17:28:52 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#define Random_Capitalize
 #define import_randint
 #include "random.hpp"
 
 using namespace std;
 
 int main() {
-	cout << randint(5, 10) << '\n';
+	cout << random.randInt(5, 10) << '\n';
 	cout << random.choice({9,3,54,8}) << '\n';
 };
