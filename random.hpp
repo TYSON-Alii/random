5cde33aea52b3043e635f19e2299066e215c56d9
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-10-23 16:41:51 +0300

    Update random.hpp

--- random.hpp
+++ random.hpp
@@ -2,16 +2,25 @@
 #include <vector>
 struct {
 	int randint(int begin, int end) { return (std::rand() % (end - begin)) + begin; };
+	int randint(int end) { return std::rand() % end; };
+	int randint() { return std::rand() % INT_MAX; };
 	template <typename T>
 	T choice(std::initializer_list<T> v) {
 		std::vector<T> l = v;
 		auto s = std::rand() % l.size();
 		return l[s];
 	};
+	template <typename T>
+	T choice(std::vector<T> v) {
+		auto s = std::rand() % v.size();
+		return v[s];
+	};
 } random;
 
 #if defined(import_randint)
 int randint(int begin, int end) { return (std::rand() % (end - begin)) + begin; };
+int randint(int end) { return std::rand() % end; };
+int randint() { return std::rand() % INT_MAX; };
 #endif
 #if defined(import_choice)
 template <typename T>
@@ -20,4 +29,10 @@ T choice(std::initializer_list<T> v) {
 	auto s = std::rand() % l.size();
 	return l[s];
 };
+template <typename T>
+T choice(std::vector<T> v) {
+	auto s = std::rand() % v.size();
+	return v[s];
+};
 #endif
+
