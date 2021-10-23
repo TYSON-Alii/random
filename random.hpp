a76ba50c8a35e0f32154f1d54a1e1f8b165b0cbe
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2021-10-23 17:00:28 +0300

    Update random.hpp

--- random.hpp
+++ random.hpp
@@ -4,6 +4,13 @@ struct {
 	int randint(int begin, int end) { return (std::rand() % (end - begin)) + begin; };
 	int randint(int end) { return std::rand() % end; };
 	int randint() { return std::rand() % INT_MAX; };
+
+	bool randbool() { return std::rand() % 2 == 0 ? false : true; };
+
+	char randchar() { return char(std::rand() % 256 - 128); };
+	char randletter() { return char(randbool() ? randint(65, 91) : randint(97,123)); };
+	char randsymbol() { return char( randbool() ? randint(33, 48) : randbool() ? randint(58, 65) : randbool() ? randint(91, 97) : randint(123, 127) ); };
+
 	template <typename T>
 	T choice(std::initializer_list<T> v) {
 		std::vector<T> l = v;
