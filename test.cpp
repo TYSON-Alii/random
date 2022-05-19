a85a5dbf2f449ad97d4a6a0b4525e2718b8d2fb4
tyson <65432197+TYSON-Alii@users.noreply.github.com>
2022-05-19 11:37:46 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
-#define Random_Capitalize
-#define import_randint
 #include "random.hpp"
 
 using namespace std;
 
 int main() {
-	cout << random.randInt(5, 10) << '\n';
-	cout << random.choice({9,3,54,8}) << '\n';
+	auto& r = random; // aliasing
+	r.randInt(5, 10);
+	random.randFloat();
+	if (random.randBool())
+		cout << random.rand<unsigned long long>(50) << '\n';
+	random.choice({9,3,54,8});
+	random.choice({"selam", "meraba", "wow"});
+	string falanke = "filanke";
+	random.choice(falanke);
+	// or random.choice("filanke"s);
+	random.randRange(50, 10);
+	cout << random.randChar() << '\n';
+	cout << random.randLetter() << '\n';
+	cout << random.randSymbol() << '\n';
 };
